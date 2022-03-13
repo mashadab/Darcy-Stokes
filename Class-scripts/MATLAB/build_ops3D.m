03733a48e88ff78ccb0e2e38a5ef9cbee2e16300
Afzal Shadab <mashadab@mit.edu>
2022-03-13 15:37:57 -0500

    3D divergence gradients made

---
+++ Class-scripts/MATLAB/build_ops3D.m
@@ -0,0 +1,71 @@
+function [D,G,C,I,M]=build_ops3D(Grid)
+% author: 
+% date: 
+% description:
+% This function computes the discrete operator matrices on a
+% staggered grid using central difference approximations. The
+% discrete gradient assumes homogeneous boundary conditions.
+% Input:
+% Grid = structure containing all pertinent information about the grid.
+% Output:
+% D = Nx by Nx+1 discrete divergence matrix 
+% G = Nx+1 by Nx discrete gradient matrix
+% C = discrete curl - not defined in 1D
+% I = Nx by Nx identity matrix
+% M = Nx+1 by Nx discrete mean matrix
+%
+% Example call:
+% >> Grid.xmin = 0; Grid.xmax = 1; Grid.Nx = 10;
+% >> Grid = build_grid(Grid);
+% >> [D,G,C,I,M]=build_ops(Grid);
+
+Nx = Grid.Nx; Nfx = Grid.Nfx;
+Ny = Grid.Ny; Nfy = Grid.Nfy;
+Nz = Grid.Nz; Nfz = Grid.Nfz;
+
+%% 1) Build sparse Divergence operator
+%Build Identities
+Ix = speye(Nx);
+Iy = speye(Ny);
+Iz = speye(Nz);
+
+% Build 3D Dx matrix
+Dx = spdiags([-ones(Nx,1) ones(Nx,1)]/Grid.dx,[0 1],Nx,Nx+1);
+Dx = kron(Iz,kron(Dx,Iy));
+
+% Build 3D Dy matrix
+Dy = spdiags([-ones(Ny,1) ones(Ny,1)]/Grid.dy,[0 1],Ny,Ny+1);
+Dy = kron(Iz,kron(Ix,Dy));
+
+% Build 3D Dz matrix
+Dz = spdiags([-ones(Nz,1) ones(Nz,1)]/Grid.dz,[0 1],Nz,Nz+1);
+Dz = kron(Dz,kron(Ix,Iy));
+
+% Full 3D discrete divergence matrix
+D = [Dx,Dy,Dz];
+                                                                            
+%% 2) Build sparse Gradient operator
+%  Interior
+G = -D';
+
+%{
+% Set natural (homogeneous Neumann) boundary conditions
+dof_f_bnd = [Grid.dof_f_xmin;Grid.dof_f_xmax]; % all dof's on boundary
+G(dof_f_bnd,:) = 0;
+%}
+
+% 3) Discrete Curl operator (not defined in 1D)
+C = [];
+
+% 4) Sparse Identity 
+I = speye(Grid.N);
+
+% 5) Sparse Mean
+% Interior
+M = 0.5*Grid.dx*abs(G);
+
+%{
+% Boundaries
+M(1,1)  = 1; 
+M(Grid.Nfx,Grid.Nx) = 1;
+%}
