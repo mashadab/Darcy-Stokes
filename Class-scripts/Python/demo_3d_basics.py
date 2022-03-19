ce3d17b13e1c9fdfce3a4a4bc8d5ec754728b099
Afzal Shadab <mashadab@mit.edu>
2022-03-18 20:11:09 -0500

    more progress

--- Class-scripts/Python/demo_3d_basics.py
+++ Class-scripts/Python/demo_3d_basics.py
@@ -2,7 +2,7 @@
 #Lecture 15 (March 10): 3D Python basics
 #Author: Mohammad Afzal Shadab and Marc Hesse
 
-'#In two dimensions we will extensively use two functions for plotting: meshgrid and reshape'
+#In two dimensions we will extensively use two functions for plotting: meshgrid and reshape
 
 #import library and modules
 import numpy as np
@@ -22,10 +22,10 @@ f = lambda x,y,z: x**2*y*z
 g = lambda x,y,z: y
 h = lambda x,y,z: z
 
-Nx =4; Ny=3; Nz=2
-x = np.linspace(1,Nx,Nx)
-y = np.linspace(Nx+1,Nx+Ny+1,Ny)
-z = np.linspace(Nx+Ny+1,Nx+Ny+Nz+1,Nz)
+Nx =4; Ny=3; Nz=2; N=Nx*Ny*Nz
+x = np.linspace(0,1,Nx)
+y = np.linspace(0,2,Ny)
+z = np.linspace(0,3,Nz)
 [X,Z,Y] = np.meshgrid(x,z,y)
 
 print('X:',X); print('Y:',Y); print('Z:',Z); print(X.shape)
@@ -42,12 +42,17 @@ arr = np.arange(27).reshape(3,3,3)
 '''
 In the matrices X and Y, the -value increases with the row index, , and the 
 -value increases with the column index,  and k  in the third direction.  
-Since we index matrices as X(k,j,i) and Y(k,j,i), and Z(k,j,i), the first index 
+Since we index matrices as X(k,i,j) and Y(k,j,i), and Z(k,i,j), the first index 
 is the -coordinate. This makes it natural to order our grid y-first - see below! 
 '''
 
 # Create and plot structured grid
 grid = pv.StructuredGrid(X,Y,Z)
+
+# Add the data values to the cell data
+grid.point_data["values"] = f(X,Y,Z).flatten(order="F")  # Flatten the array!
+
+grid.plot(show_edges=True,show_grid=True)
 
 # Add the data values to the cell data
 grid.point_data["values"] = g(X,Y,Z).flatten(order="F")  # Flatten the array!
@@ -56,9 +61,29 @@ grid.plot(show_edges=True,show_grid=True)
 
 
 
+#####################################################################################
+#Reshaping
+
+print(X)
+print("Flatten X (first way):",np.transpose([X.flatten()]))
+print("Flatten X (second way):",np.reshape(X,(N,-1)))
 
 
+'''
+Of course reshape() is the more general, it allows you to transform X into 
+any matrix or vector with the same number of elements
 
+1) From vector to matrix
+Suppose the solution is given by g = g(x)
+'''
+
+print('Norm for reshaped x-coordinate',np.linalg.norm(X - np.reshape(X.flatten(),(Nz,Nx,Ny))))
+
+soln = g(np.transpose([X.flatten()]),np.transpose([Y.flatten()]),np.transpose([Z.flatten()]))
+
+SOLN = soln.reshape(Nz,Nx,Ny)
+
+print('Norm for reshaped solution',np.linalg.norm(g(X,Y,Z)-SOLN))
 '''
 #parameters
 HD = 0.1  #dimensionless domain length
