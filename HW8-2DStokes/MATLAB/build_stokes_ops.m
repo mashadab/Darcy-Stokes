bae68f499c22b51c7f1b38900a6ffecb1588ca7c
Afzal Shadab <mashadab@mit.edu>
2022-04-09 18:28:22 -0500

    Working Stokes operator

---
+++ HW8-2DStokes/MATLAB/build_stokes_ops.m
@@ -0,0 +1,35 @@
+function [D,Edot,Dp,Gp,Z,I]=build_stokes_ops(Grid) % repo
+% authors: Mohammad Afzal Shadab, Marc Hesse, Evan Carnahan
+% date: 9 April 2021
+
+%% Build operators for each variable
+[Dp,Gp,~,Ip,~] = build_ops2D(Grid.p);
+[Dx,Gx,~,Ix,~] = build_ops2D(Grid.x);
+[Dy,Gy,~,Iy,~] = build_ops2D(Grid.y);
+
+%% Extract x and y components of the velocity operators
+Gxx = Gx(1:Grid.x.Nfx,:); Gxy = Gx(Grid.x.Nfx+1:Grid.x.Nf,:);
+Gyx = Gy(1:Grid.y.Nfx,:); Gyy = Gy(Grid.y.Nfx+1:Grid.y.Nf,:);
+
+Dxx = Dx(:,1:Grid.x.Nfx); Dxy = Dx(:,Grid.x.Nfx+1:Grid.x.Nf);
+Dyx = Dy(:,1:Grid.y.Nfx); Dyy = Dy(:,Grid.y.Nfx+1:Grid.y.Nf);
+
+% Zero blocks
+Zxy = spalloc(Grid.x.Nfx,Grid.y.N,0);
+Zyx = spalloc(Grid.y.Nfy,Grid.x.N,0);
+
+%% Assemble Stokes operators
+% Symmetric derivative
+Edot = [Gxx,Zxy; ...
+        Zyx,Gyy; ...
+      Gxy/2,Gyx/2];
+
+ % Divergence of deviatoric stess tensor
+D = [Dxx, Zyx', Dxy; ...
+     Zxy',Dyy , Dyx];
+ 
+ % Zero block for the system matrix L
+ Z = spalloc(Grid.p.N,Grid.p.N,0);
+
+ % Identity for all dof's
+ I = speye(Grid.N);
---
+++ HW8-2DStokes/MATLAB/build_stokes_ops_implementation.m
@@ -0,0 +1,21 @@
+mu = 1; % viscosity - value does not matter here
+
+% Definition of pressure grid
+Gridp.xmin = 0; Gridp.xmax = 1; Gridp.Nx = 5;
+Gridp.ymin = 0; Gridp.ymax = 1; Gridp.Ny = 5;
+
+Grid = build_stokes_grid(Gridp);
+[D,Edot,Dp,Gp,Z,I] = build_stokes_ops(Grid);
+
+A = 2*mu*D*Edot; % 
+L = [A, -Gp;...
+     Dp, Z];
+
+subplot 141
+spy(D), title 'D'
+subplot 142
+spy(Edot), title 'Edot'
+subplot 143
+spy(A), title 'A'
+subplot 144
+spy(L), title 'L'
